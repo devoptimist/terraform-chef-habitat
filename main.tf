6737236d8471550a2c05affe2aae0ce06343c992
devoptimist <sbrown@chef.io>
2019-08-25 22:08:12 +0100

    updated effortless bootstrap module version

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ locals {
 
 module "effortless_bootstrap_hab" {
   source            = "devoptimist/effortless-bootstrap/chef"
-  version           = "0.0.3"
+  version           = "0.0.4"
   ips               = var.ips
   instance_count    = local.instance_count
   user_name         = var.user_name

8254bf2a47b267082c53c5f492800714789f3420
devoptimist <sbrown@chef.io>
2019-08-25 21:31:24 +0100

    relaxed hab_services map constraints, updated effortless bootstrap module version

--- .terraform/modules/effortless_bootstrap_hab/devoptimist-terraform-chef-effortless-bootstrap-d3405d1/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "module_hook" {
-  description = "A list of random string outputs from each connection this module made. Used to enforce the order in which the module is run"
-  value       = data.null_data_source.module_hook.*.outputs.data
-}

f8c13c6e82d0e86c006e158e393d41e8700fd8e6
srb3 <sbrown@chef.io>
2020-02-23 17:48:29 +0000

    Using latest effotless module

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ locals {
 
 module "effortless_bootstrap_hab" {
   source            = "srb3/effortless-bootstrap/chef"
-  version           = "0.0.5"
+  version           = "0.0.6"
   ips               = var.ips
   instance_count    = local.instance_count
   user_name         = var.user_name

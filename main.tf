d6e4a23f7b37447ff5fbf4c9a0f2c5fb0e8cd341
srb3 <sbrown@chef.io>
2020-02-28 00:23:53 +0000

    use version v0.0.14 of effortless bootstrap terraform module

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ locals {
 
 module "effortless_bootstrap_hab" {
   source            = "srb3/effortless-bootstrap/chef"
-  version           = "0.0.13"
+  version           = "0.0.14"
   ips               = var.ips
   instance_count    = local.instance_count
   user_name         = var.user_name

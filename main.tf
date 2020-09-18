807b2e14b42711a5fc5ee1d6d660a21240b960f5
srb3 <sbrown@chef.io>
2020-09-18 15:30:47 +0100

    upgrade to tf v13

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ locals {
 
 module "effortless_bootstrap_hab" {
   source            = "srb3/effortless-bootstrap/chef"
-  version           = "0.0.15"
+  version           = "0.13.0"
   ips               = var.ips
   instance_count    = local.instance_count
   user_name         = var.user_name
